4c4f75d179c0db3832038958d5babd0642c47812
Sem Onyalo <oonyalo@gmail.com>
2018-02-19 13:51:16 -0500

    added project markers on project map page

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,7 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
+import load from 'little-loader';
 import './Projects.css';
 
 export default class Projects extends Component {
@@ -7,12 +10,13 @@ export default class Projects extends Component {
 
         this.state = {
             mapHeight: this.getViewportHeight(),
-            mapRegion: this.getMapRegion()
+            mapRegion: this.getMapRegion(),
+            projectMapsLoading: false
         };
 
         this._mainNavHeight = 56;
-        this._mapUrl = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAgISl38x3fEnLXxn_2VYumF9b9RsPFVHQ';
         this.updateMapDimensions = this.updateMapDimensions.bind(this);
+        this.loadProjectsMapCallback = this.loadProjectsMapCallback.bind(this);
     }
 
     render() {
@@ -20,23 +24,15 @@ export default class Projects extends Component {
             height: this.state.mapHeight + 'px'
         };
 
-        var mapUrl = this._mapUrl + "&q=" + this.state.mapRegion;
-
-        return (
-            <iframe 
-                src={mapUrl}
-                className="projects-map"
-                marginHeight="0"
-                marginWidth="0"
-                frameBorder="0"
-                scrolling="no"
-                style={mapStyle}>Loading map...</iframe>
-        );
+        return (<div id="projectsMap" className="projects-map"></div>);
     }
 
     getMapRegion() {
         // TODO: pull from datasource or drop down menu selected by user
-        return "Toronto+ON";
+        return {
+            lat: 43.6532,
+            lng: -79.3832
+        }; // Toronto, ON
     }
 
     getViewportHeight() {
@@ -50,11 +46,60 @@ export default class Projects extends Component {
     }
 
     updateMapDimensions() {
-        this.setState({ mapHeight: this.getViewportHeight() - this._mainNavHeight });
+        this.setState({
+            mapHeight: this.getViewportHeight() - this._mainNavHeight
+        });
+    }
+
+    loadProjectsMapCallback(googleMaps) {
+        document.getElementById('projectsMap').style.height = this.state.mapHeight + 'px';
+        
+        var map = new googleMaps.Map(document.getElementById('projectsMap'), {
+            center: this.state.mapRegion,
+            zoom: 10
+        });
+
+        // TODO: pull marker locations from datasource
+        var marker = new googleMaps.Marker({
+            map: map,
+            position: { lat: 43.787476, lng: -79.275253 } // 23 and 25 Glen Watford Drive Land Development Project
+        });
+
+        marker = new googleMaps.Marker({
+            map: map,
+            position: { lat: 43.651937, lng: -79.363606 } // First Parliment Development Project
+        });
+    }
+
+    waitForGoogleAPI() {
+        setTimeout(() => {
+            if (!window.projectMapsLoading) {
+                this.loadProjectsMapCallback(window.google.maps);
+            }
+        }, 300);
     }
 
     componentDidMount() {
         window.addEventListener("resize", this.updateMapDimensions);
+
+        if (typeof window.google === 'undefined') {
+            // TODO: move to config file
+            const apiKey = 'AIzaSyAgISl38x3fEnLXxn_2VYumF9b9RsPFVHQ';
+
+            if (window.projectMapsLoading) {
+                this.waitForGoogleAPI();
+            } else {
+                window.projectMapsLoading = true;
+                load(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`, (err) => {
+                    if (!err) {
+                        window.projectMapsLoading = false;
+                        this.loadProjectsMapCallback(window.google.maps);
+                    }
+                });
+            }
+        } else {
+            this.loadProjectsMapCallback(window.google.maps);
+        }
     }
 
     componentWillUnmount() {
